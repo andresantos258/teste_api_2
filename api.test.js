c1a20eb224628dd5d2be11691092551acbd19ea6
andresantos258 <130004923+andresantos258@users.noreply.github.com>
2024-03-18 23:29:20 +0000

    teste15

--- api.test.js
+++ api.test.js
@@ -7,3 +7,15 @@ describe('Teste GET /users', () => {
     expect(response.statusCode).toBe(200);
   });
 });
+
+describe('Teste POST /users', () => {
+  it('deve responder com status 201', async () => {
+    const response = await supertest(app).post('/users', {name: "Fulano de tal", email:"fulano@getMaxListeners.com"});
+    expect(response.statusCode).toBe(201);
+  });
+});
+
+afterAll(() => {
+  app.server.close();
+  app.connection.end();
+})

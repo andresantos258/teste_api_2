6eb4be58e0de62ce40155f0c049aacc87bfdc04a
andresantos258 <130004923+andresantos258@users.noreply.github.com>
2024-03-19 00:26:17 +0000

    teste21

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const port = 3000;
 // Configuração do MySQL
 const connection = mysql.createConnection({
   host: '127.0.0.1',
-//user: 'root', // Seu nome de usuário do MySQL
+  user: 'root', // Seu nome de usuário do MySQL
 //password: 'aloha', // Sua senha do MySQL
 //database: 'usersdb'
-  user: process.env.MYSQL_USER, // Usa variáveis de ambiente
+  //user: process.env.MYSQL_USER, // Usa variáveis de ambiente
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE
 

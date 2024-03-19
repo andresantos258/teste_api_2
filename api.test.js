53360051749f60f34afef1e986db8d36ba954150
andresantos258 <130004923+andresantos258@users.noreply.github.com>
2024-03-19 00:07:47 +0000

    teste19

--- api.test.js
+++ api.test.js
@@ -5,12 +5,18 @@ describe('Teste GET /users', () => {
   it('deve responder com status 200', async () => {
     const response = await supertest(app).get('/users');
     expect(response.statusCode).toBe(200);
+
+    expect(response.body).toBeInstanceOf(Array);
+    response.body.array.forEach(element => {
+      expect(element).toHaveProperty("name");
+    });
+
   });
 });
 
 describe('Teste POST /users', () => {
   it('deve responder com status 201', async () => {
-    const response = await supertest(app).post('/users', {name: "Fulano de tal", email:"fulano@getMaxListeners.com"});
+    const response = await supertest(app).post('/users', {name: "Fulano de tal", email:"fulano@gmail.com"});
     expect(response.statusCode).toBe(201);
   });
 });
